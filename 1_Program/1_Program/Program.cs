da68487b353addcddb835d5ad42aac46d23c1694
Zumber08 <83783294+Zumber08@users.noreply.github.com>
2023-02-28 19:29:09 +0100

    UZUPEŁNIENIE

--- 1_Program/1_Program/Program.cs
+++ 1_Program/1_Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.WriteLine("\n-------------------------\n");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("WITAJ W KURSIE");
+Console.WriteLine("JESTEM DK");
 Console.WriteLine("\n-------------------------\n");
