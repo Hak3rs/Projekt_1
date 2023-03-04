4d55f0b9dc0dc0c94915ed29ec8f63c9daf40ba4
Zumber08 <83783294+Zumber08@users.noreply.github.com>
2023-03-04 23:19:08 +0100

    Zadanie_6

---
+++ Zad_6/Zad_6/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_6
+{
+    public class Employee
+    {
+        public static string firma = "HAK3RS";
+        //lista punktów
+        private List<int> punkty = new List<int>();
+        //konstruktor
+        public Employee(string imie1, string nazwisko1, int wiek1)
+        {
+            this.imie = imie1;
+            this.nazwisko = nazwisko1;
+            this.wiek = wiek1;
+        }
+
+        //propetries
+        public string imie
+        {
+            get;
+            private set;
+        }
+        public string nazwisko
+        {
+            get;
+            private set;
+        }
+        public int wiek
+        {
+            get;
+            private set;
+        }
+
+        //punkty
+        public void DodajPKT(int pkt)
+        {
+            this.punkty.Add(pkt);
+        }
+        public int suma_pkt
+        {
+            get
+            {
+                return punkty.Sum();
+            }
+        }
+    }
+}
---
+++ Zad_6/Zad_6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata;
+using Zad_6;
+//pracownicy
+Employee p1 = new Employee("Kamil", "Maciaszczyk", 26);
+Employee p2 = new Employee("Kasia", "Miller", 31);
+Employee p3 = new Employee("Krzysztof", "Ksiazek", 25);
+
+List<Employee> pracownicy = new List<Employee>();
+pracownicy.Add(p1);
+pracownicy.Add(p2);
+pracownicy.Add(p3);
+
+//oceny
+p1.DodajPKT(4);
+p1.DodajPKT(5);
+p1.DodajPKT(7);
+p1.DodajPKT(10);
+p1.DodajPKT(3);
+
+p2.DodajPKT(6);
+p2.DodajPKT(8);
+p2.DodajPKT(9);
+p2.DodajPKT(1);
+p2.DodajPKT(3);
+
+p3.DodajPKT(6);
+p3.DodajPKT(8);
+p3.DodajPKT(9);
+p3.DodajPKT(5);
+p3.DodajPKT(7);
+
+int punkty = 0;
+var corporation = Employee.firma;
+Employee NajlepszyPracownik = null;
+
+foreach (var pr in pracownicy)
+{
+    if (pr.suma_pkt > punkty)
+    {
+        punkty = pr.suma_pkt;
+        NajlepszyPracownik = pr;
+    }
+}
+Console.WriteLine("\nFirma: \t" + corporation);
+Console.WriteLine("Imie: \t" + NajlepszyPracownik.imie);
+Console.WriteLine("Nazwisko: \t" + NajlepszyPracownik.nazwisko);
+Console.WriteLine("Wiek: \t" + NajlepszyPracownik.wiek);
+Console.WriteLine("Punkty: \t" + NajlepszyPracownik.suma_pkt);
